d6fd42416f98e2338e18d87d414e4b65cb218ade
Sudeep Batra <sudeep.batra@att.com>
2022-10-26 12:06:22 -0500

    resync

--- config/database.php
+++ config/database.php
@@ -11,8 +11,8 @@ return [
     |
     | Here you may specify which of the database connections below you wish
     | to use as your default connection for all database work. Of course
-    | you may use many connections at once using the Database library.
-    | No change.
+    | you may use many connections at once using the Database library..
+    | 
     */
 
     'default' => env('DB_CONNECTION', 'mysql'),
